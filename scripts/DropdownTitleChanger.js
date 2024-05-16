a940a4673f5ba26c911ed411bd0ed4215a23234c
Yakov Zilberman <zilberman.yakoff@yandex.ru>
2024-05-16 10:38:35 +0500

    fix: сделал, чтобы текст заголовка страницы передавался при инициализации класса DropdownTitleChanger, исправил шрифт заголовка страницы

--- scripts/DropdownTitleChanger.js
+++ scripts/DropdownTitleChanger.js
@@ -1,14 +1,15 @@
 import { Dropdown } from './Dropdown.js'
 
 class DropdownTitleChanger extends Dropdown {
-  constructor(dropdownSelector, pageTitleSelector) {
+  constructor(dropdownSelector, pageTitleSelector, titleText) {
     super(dropdownSelector)
     this._pageTitleElement = document.querySelector(pageTitleSelector)
+    this._titleText = titleText
   }
 
   _optionClick(option) {
     super._optionClick(option)
-    this._pageTitleElement.textContent = `Заголовок ${option.getAttribute('data-value')}`
+    this._pageTitleElement.textContent = `${this._titleText} ${option.getAttribute('data-value')}`
   }
 }
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,9 @@
 import { DropdownTitleChanger } from './DropdownTitleChanger.js';
 import { DropdownWithAlert } from './DropdownWithAlert.js';
 
-const dropdownWithTitleChange = new DropdownTitleChanger('.header__dropdown_type_change-title', '.main-section__title')
+const titleText = document.querySelector('.main-section__title').textContent
+
+const dropdownWithTitleChange = new DropdownTitleChanger('.header__dropdown_type_change-title', '.main-section__title', titleText)
 dropdownWithTitleChange.setEventListeners()
 
 const dropdownWithAlert = new DropdownWithAlert('.header__dropdown_type_alert')
